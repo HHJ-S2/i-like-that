0d0370a79d8be6f6d9df2dd0b99be4aba6c9bb33
HHJ-S2 <hhyojeong0122@gmail.com>
2024-04-10 22:56:14 +0900

    feat: Main storyboard 삭제 및 네비게이션 설정

--- ilikethat/SceneDelegate.swift
+++ ilikethat/SceneDelegate.swift
@@ -11,12 +11,35 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
   var window: UIWindow?
 
-
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-    // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-    // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-    // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-    guard let _ = (scene as? UIWindowScene) else { return }
+    guard let windowScene = (scene as? UIWindowScene) else { return }
+    
+    window = UIWindow(windowScene: windowScene)
+    
+    // 탭바컨트롤러의 생성
+    let tabBarVC = UITabBarController()
+    
+    // 첫번째 화면은 네비게이션컨트롤러로 만들기 (기본루트뷰 설정)
+    let vc1 = UINavigationController(rootViewController: ItemsViewController())
+    let vc2 = SettingsViewController()
+    
+    // 탭바 이름들 설정
+    vc1.title = "Items"
+    vc2.title = "Settings"
+    
+    // 탭바로 사용하기 위한 뷰 컨트롤러들 설정
+    tabBarVC.setViewControllers([vc1, vc2], animated: false)
+    tabBarVC.modalPresentationStyle = .fullScreen
+    tabBarVC.tabBar.backgroundColor = .white
+    
+    // 탭바 이미지 설정 (이미지는 애플이 제공하는 것으로 사용)
+    guard let items = tabBarVC.tabBar.items else { return }
+    items[0].image = UIImage(systemName: "heart.fill")
+    items[1].image = UIImage(systemName: "ellipsis")
+    
+    // 기본루트뷰를 탭바컨트롤러로 설정
+    window?.rootViewController = tabBarVC
+    window?.makeKeyAndVisible()
   }
 
   func sceneDidDisconnect(_ scene: UIScene) {
